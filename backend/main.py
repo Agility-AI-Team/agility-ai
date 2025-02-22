30ef3035dc37e1726bcc691c32251c836db98949
Chris Yoo <c.yoo@student.unsw.edu.au>
2025-02-22 16:13:20 +1100

    jira backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from jira import jira_create_issue, jira_edit_issue, jira_get_issues
+from jira import jira_create_issue, jira_edit_issue, jira_get_issue, jira_get_issue_transitions, jira_get_issues, jira_transition_issue
 from pydantic import BaseModel
 from fastapi import FastAPI
 from uuid import uuid4
@@ -39,8 +39,11 @@ async def get_issues_for_user(meeting_id: str):
     issues = jira_get_issues(project_key=jira_project_key, assignee_id=jira_user_id)
     return { "issues": issues }
 
+@app.get("/meeting/{meeting_id}/api/jira/getIssue")
+async def get_issue(meeting_id: str, issue_id: str):
+    issue = jira_get_issue(issue_id=issue_id)
+    return { "issue": issue }
 class CreateIssueRequest(BaseModel):
-    project_key: str
     title: str
     description: str
     assignee_id: Optional[str] = None
@@ -48,15 +51,42 @@ class CreateIssueRequest(BaseModel):
 
 @app.post("/meeting/{meeting_id}/api/jira/createIssue")
 async def create_issue(meeting_id: str, request: CreateIssueRequest):
-    result = jira_create_issue(project_key=request.project_key, summary=request.title, description=request.description, assignee_id=request.assignee_id, due_date=request.due_date)
+    result = jira_create_issue(
+        project_key=jira_project_key,
+        summary=request.title,
+        description=request.description,
+        assignee_id=request.assignee_id,
+        due_date=request.due_date,
+    )
     return result
 
 class EditIssueRequest(BaseModel):
-    pass
+    issue_id: str
+    summary: Optional[str] = None
+    description: Optional[str] = None
+    assignee_id: Optional[str] = None
+    due_date: Optional[str] = None # YYYY-MM-DD
 
 @app.put("/meeting/{meeting_id}/api/jira/editIssue")
 async def edit_issue(meeting_id: str, request: EditIssueRequest):
-    result = jira_edit_issue()
+    result = jira_edit_issue(
+        issue_id=request.issue_id,
+        status=request.status,
+        summary=request.summary,
+        description=request.description,
+        assignee_id=request.assignee_id,
+        due_date=request.due_date,
+    )
+    return result
+
+@app.get("/meeting/{meeting_id}/api/jira/getIssueTransitions")
+async def get_issue_transitions(meeting_id: str, issue_id: str):
+    transitions = jira_get_issue_transitions(issue_id=issue_id)
+    return {"transitions": transitions}
+
+@app.post("/meeting/{meeting_id}/api/jira/transitionIssue")
+async def change_issue_status(meeting_id: str, issue_id: str, transition_id: str):
+    result = jira_transition_issue(issue_id=issue_id, transition_id=transition_id)
     return result
 
 # GitHub
